092b281eca478d4a305d732ba6b3d290083e0913
Vayp <vaypeks@gmail.com>
2019-02-24 18:29:13 +0200

    Function which calculates how many love triangles phenomenons take place

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let firstAmorous, secondAmorous, thirdAmorous;
+  let lovers = 0
+  for(firstAmorous = 0; firstAmorous <= preferences.length; firstAmorous++ ){
+    secondAmorous = preferences[firstAmorous] - 1;
+    thirdAmorous = preferences[secondAmorous] -1;
+    if ((preferences[firstAmorous] > preferences.length)|(firstAmorous + 1 == preferences[firstAmorous])) firstAmorous++
+      if(preferences[thirdAmorous]-1 == firstAmorous) lovers++;
+  }
+  return Math.round(lovers/3);
 };
